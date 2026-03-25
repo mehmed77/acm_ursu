aa52b4dfcd2248fe1eb2e34bdc608738244fd687
Muhammad Habibullayev <mehmed_77@mail.ru>
2026-03-25 14:26:28 +0500

    edit birnimalar

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,27 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 export default function Layout() {
-    const location = useLocation();
-
     return (
         <div className="min-h-screen flex flex-col">
             <Navbar />
             <main className="flex-1" style={{ paddingTop: 10 }}>
-                <AnimatePresence mode="sync">
-                    <motion.div
-                        key={location.pathname}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={{ duration: 0.15, ease: 'easeOut' }}
-                    >
-                        <Outlet />
-                    </motion.div>
-                </AnimatePresence>
+                <Outlet />
             </main>
             <Footer />
         </div>
